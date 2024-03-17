5ca51e3502cda0f1bb6bab6489e7691c6562262e
aria-1337 <aria.lopez.dev@gmail.com>
2024-03-16 20:46:29 -0700

    Init http server

--- src/main.go
+++ src/main.go
@@ -1,12 +1,39 @@
 package main
 
 import (
-    "github.com/syndtr/goleveldb/leveldb"
+    "fmt"
+	"net/http"
+    "math/rand"
+    "time"
+
+	"github.com/syndtr/goleveldb/leveldb"
 )
 
 type App struct {
     db *leveldb.DB
 }
 
+func (a *App) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, "hey\n")
+}
+
+
 func main() {
+    // Handle multiple transport
+    http.DefaultTransport.(*http.Transport).MaxIdleConnsPerHost = 100
+    rand.Seed(time.Now().Unix())
+
+    // connect to level db 
+    db, err := leveldb.OpenFile("test", nil)
+    if err != nil {
+        panic(fmt.Sprintf("Could not establish connection to LevelDB: %s", err))
+    }
+    defer db.Close()
+
+    // Serve
+    a:= App{db: db}
+
+
+    fmt.Println("kv running at localhost:3000")
+    http.ListenAndServe(":3000", &a)
 }
