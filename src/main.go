6dbfb95a03dcc99306b8f6ce1ef228c64d19e22d
aria-1337 <aria.lopez.dev@gmail.com>
2024-03-16 22:08:03 -0700

    Refactored file lookup and kinda fixed HTTP status code handling

--- src/main.go
+++ src/main.go
@@ -20,42 +20,45 @@ type App struct {
     db *leveldb.DB
 }
 
+func (a *App) getRecord(key string) ([]byte, int) {
+    data, err := a.db.Get([]byte(key), nil)
+    code := 200
+    if err != leveldb.ErrNotFound  && err != nil {
+        code = 500
+    }
+    if err == leveldb.ErrNotFound {
+        code = 404
+    }
+    return data, code
+}
+
+// POST - 201=success 403=forbidden overwrite 
 func (a *App) set(w http.ResponseWriter, body *Request) {
-    err := a.db.Put([]byte(body.Key), []byte(fmt.Sprint(body.Value)), nil)
-
-    // TODO: Better error handling
-    if err != nil {
-        w.WriteHeader(http.StatusInternalServerError)
-        w.Write([]byte("500 - Example error message"))
-    } else {
-        w.WriteHeader(http.StatusAccepted)
-        w.Write([]byte("201 - OK"))
+    _, code := a.getRecord(body.Key)
+    if code == 200 {
+        w.WriteHeader(403)
+        return
     }
+    a.db.Put([]byte(body.Key), []byte(fmt.Sprint(body.Value)), nil)
+    w.WriteHeader(201)
 }
 
+// GET - 200=success 404=record doesnt exist
 func (a *App) get(w http.ResponseWriter, body *Request) {
-    data, err := a.db.Get([]byte(body.Key), nil)
-    
-    if err != nil {
-        w.WriteHeader(http.StatusInternalServerError)
-        w.Write([]byte("500 - Example error message"))
-    } else {
-        w.WriteHeader(http.StatusAccepted)
-        w.Write([]byte(data))
-    }
+    data, code := a.getRecord(body.Key)
+    w.WriteHeader(code)
+    w.Write([]byte(data))
 }
 
+// DELETE - 204=success 409=not deleted
 func (a *App) delete(w http.ResponseWriter, body *Request) {
-    err := a.db.Delete([]byte(body.Key), nil)
-
-    // TODO: Better error handling
-    if err != nil {
-        w.WriteHeader(http.StatusInternalServerError)
-        w.Write([]byte("500 - Example error message"))
-    } else {
-        w.WriteHeader(http.StatusAccepted)
-        w.Write([]byte("201 - OK"))
+    _, code := a.getRecord(body.Key)
+    if code == 404 {
+        w.WriteHeader(409)
+        return
     }
+    a.db.Delete([]byte(body.Key), nil)
+    w.WriteHeader(204)
 }
 
 func (a *App) ServeHTTP(w http.ResponseWriter, r *http.Request) {
